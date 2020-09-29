100cedff1c1a05140289ecd8ee21623f98585276
Wojciech Ossowski <wojciech.ossowski@fs.utc.com>
2020-09-29 23:01:13 +0200

    Adding basic software and configuration

---
+++ setup.sh
@@ -0,0 +1,23 @@
+# Install homebrew 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install zsh and plugins
+brew install zsh nvm git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Add Following stuff to ~/.bash_profile:
+# export NVM_DIR="$HOME/.nvm"
+#    [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && . "$(brew --prefix)/opt/nvm/nvm.sh" # This loads nvm
+#    [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && . "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+
+# Install additional software
+brew cask install iterm2 visual-studio-code slate clipy
+
+# Copy configuration
+cp .zshrc ~/.zshrc
+cp .slate ~/.slate
+
+# Make zsh default terminal
+chsh -s /bin/zsh
+
+# Reset terminal
