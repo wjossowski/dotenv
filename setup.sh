169756acb6a8048fec8d82560e8dbd1826062caa
Wojciech Ossowski <w.j.ossowski@gmail.com>
2022-07-25 21:35:38 +0200

    Added qmk

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install zsh and plugins
-brew install zsh nvm git neofetch fzf qmk/qmk/qmk
+brew install zsh nvm git neofetch fzf
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
@@ -13,3 +13,14 @@ git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerl
 
 # Install additional software
 brew cask install iterm2 visual-studio-code slate clipy 1password spotify karabiner-elements
+
+# QMK Keyboard Setup
+brew install qmk/qmk/qmk
+
+# Fetch Submodules
+git submodule update
+
+# Setup Keyboard firmware
+qmk setup wjossowski/qmk_firmware ./qmk
+qmk config user.keymap=wjossowski
+qmk config user.keyboard=splitkb/kyria/rev2
